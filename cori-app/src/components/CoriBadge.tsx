fef24997bb0388ff10159266473a88e56ce3a30a
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-04-01 08:56:04 +0200

    Created CoriBadge Component

---
+++ cori-app/src/components/CoriBadge.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+interface BadgeProps {
+  color?: "green" | "black" | "yellow" | "red" | "blue" | "white" | "orange";
+  size?: "x-small" | "small" | "medium" | "large";
+  text: string;
+}
+
+function CoriBadge({ color, size, text }: BadgeProps) {
+  let textSize: string;
+  let pSize: string;
+  let bgColor = "bg-corigreen-500";
+
+  // Determine the color of the badge
+  switch (color) {
+    case "green":
+      bgColor = "bg-corigreen-500";
+      break;
+    case "black":
+      bgColor = "bg-zinc-900";
+      break;
+    case "yellow":
+      bgColor = "bg-yellow-300";
+      break;
+    case "red":
+      bgColor = "bg-red-400";
+      break;
+    case "blue":
+      bgColor = "bg-cyan-600";
+      break;
+    case "white":
+      bgColor = "bg-warmstone-50";
+      break;
+    case "orange":
+      bgColor = "bg-amber-600";
+      break;
+    default:
+      bgColor = "bg-corigreen-500";
+      break;
+  }
+
+  // Set the size of the badge
+  switch (size) {
+    case "x-small":
+      textSize = "text-sm";
+      pSize = "py-1 px-2";
+      break;
+    case "small":
+      textSize = "text-sm";
+      pSize = "py-1 px-3";
+      break;
+    case "medium":
+      textSize = "text-base";
+      pSize = "py-2 px-3";
+      break;
+    case "large":
+      textSize = "text-lg";
+      pSize = "py-2 px-3";
+      break;
+    default:
+      textSize = "text-base";
+      pSize = "py-2 px-3";
+      break;
+  }
+
+  return (
+    <div
+      className={`${bgColor} ${pSize} h-fit ${
+        size === "medium" || size === "large" ? "rounded-xl" : "rounded-lg"
+      }`}
+    >
+      <p
+        className={`${textSize} ${
+          color === "yellow" || color === "white" ? "text-zinc-900" : "text-warmstone-50"
+        }`}
+      >
+        {text}
+      </p>
+    </div>
+  );
+}
+
+export default CoriBadge;
--- cori-app/src/pages/Reference.tsx
+++ cori-app/src/pages/Reference.tsx
@@ -1,10 +1,13 @@
 // Import components
 import CoriBtn from "../components/buttons/CoriBtn";
+import CoriCircleBtn from "../components/buttons/CoriCircleBtn";
+import CoriBadge from "../components/CoriBadge";
 
 // Import Icons
 import DeleteIcon from "@mui/icons-material/Delete";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AddIcon from "@mui/icons-material/Add";
 
 function ReferencePage() {
   return (
@@ -12,33 +15,54 @@ function ReferencePage() {
       <h1 className="text-3xl font-bold mb-6 text-zinc-900">Custom Components Reference</h1>
 
       {/* CoriBtns */}
-      <h3 className="text-xl font-bold mb-2">CoriBtn</h3>
-      <div className="flex flex-col gap-2">
+      <div>
+        <h3 className="text-xl font-bold mb-2">CoriBtn</h3>
         <div className="flex flex-col gap-2">
-          <div className="flex gap-2">
-            <CoriBtn primary style="default">
-              Primary Default
-            </CoriBtn>
-            <CoriBtn primary style="black">
-              Primary Black
-            </CoriBtn>
-            <CoriBtn primary style="red">
-              Primary Red with Icon <DeleteIcon />
-            </CoriBtn>
-            <CoriBtn primary iconOnly>
-              <ArrowBackIcon />
-            </CoriBtn>
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+              <CoriBtn primary style="default">
+                Primary Default
+              </CoriBtn>
+              <CoriBtn primary style="black">
+                Primary Black
+              </CoriBtn>
+              <CoriBtn primary style="red">
+                Primary Red with Icon <DeleteIcon />
+              </CoriBtn>
+              <CoriBtn primary iconOnly>
+                <ArrowBackIcon />
+              </CoriBtn>
+            </div>
+            <div className="flex gap-2">
+              <CoriBtn secondary>
+                <InsertEmoticonIcon /> Secondary Default with Icon
+              </CoriBtn>
+              <CoriBtn secondary style="black">
+                Secondary Black
+              </CoriBtn>
+              <CoriBtn secondary style="red">
+                Secondary Red
+              </CoriBtn>
+              <CoriCircleBtn icon={<AddIcon />} />
+              <CoriCircleBtn style="black" icon={<AddIcon />} />
+              <CoriCircleBtn style="red" icon={<AddIcon />} />
+            </div>
           </div>
-          <div className="flex gap-2">
-            <CoriBtn secondary>
-              <InsertEmoticonIcon /> Secondary Default with Icon
-            </CoriBtn>
-            <CoriBtn secondary style="black">
-              Secondary Black
-            </CoriBtn>
-            <CoriBtn secondary style="red">
-              Secondary Red
-            </CoriBtn>
+          <div className="mt-4">
+            <h3 className="text-xl font-bold mb-2">CoriBadge</h3>
+            <div className="flex gap-2">
+              <CoriBadge text="Large" size="large" />
+              <CoriBadge text="Medium" size="medium" />
+              <CoriBadge text="Small" size="small" />
+              <CoriBadge text="X-Small" size="x-small" />
+              <CoriBadge text="Green" size="small" color="green" />
+              <CoriBadge text="Black" size="small" color="black" />
+              <CoriBadge text="Yellow" size="small" color="yellow" />
+              <CoriBadge text="Red" size="small" color="red" />
+              <CoriBadge text="Blue" size="small" color="blue" />
+              <CoriBadge text="White" size="small" color="white" />
+              <CoriBadge text="Orange" size="small" color="orange" />
+            </div>
           </div>
         </div>
       </div>
