b836a2c58d72517779b1d429900bc40f027817f1
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-04-17 11:26:29 +0200

    Create CreateUnlinkedEquipModal

---
+++ cori-app/src/components/modals/CreateUnlinkedEquipModal.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Modal, Button, Form, Input, Select, DatePicker } from "antd";
+
+interface CreateUnlinkedEquipModalProps {
+  showModal: boolean;
+  setShowModal: (show: boolean) => void;
+}
+
+function CreateUnlinkedEquipModal({ showModal, setShowModal }: CreateUnlinkedEquipModalProps) {
+  return (
+    <Modal
+      title={<h2 className="text-zinc-900 font-bold text-3xl">Create Equipment Item</h2>}
+      open={showModal}
+      onCancel={() => setShowModal(false)}
+      width={600}
+      styles={{
+        header: {
+          paddingLeft: 40,
+          paddingRight: 40,
+          paddingTop: 40,
+        },
+        body: {
+          paddingTop: 20,
+          padding: 40,
+        },
+        footer: {
+          paddingLeft: 40,
+          paddingRight: 40,
+          paddingBottom: 40,
+        },
+      }}
+      footer={[
+        <Button key="cancel" onClick={() => setShowModal(false)}>
+          Cancel
+        </Button>,
+        <Button key="create" type="primary" onClick={() => setShowModal(false)}>
+          Create Item
+        </Button>,
+      ]}
+    >
+      <Form layout="vertical" variant="filled" className="flex flex-col">
+        <Form.Item name="equipmentName" label="Equipment Name">
+          <Input type="text" />
+        </Form.Item>
+        <Form.Item name="deviceType" label="Device Type">
+          <Select>
+            <Select.Option value="cellphone">Cellphone</Select.Option>
+            <Select.Option value="tablet">Tablet</Select.Option>
+            <Select.Option value="laptop">Laptop</Select.Option>
+            <Select.Option value="monitor">Monitor</Select.Option>
+            <Select.Option value="headset">Headset</Select.Option>
+            <Select.Option value="keyboard">Keyboard</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="condition" label="Condition">
+          <Select>
+            {/* New = 0, Good = 1, Decent = 2, Used = 3 */}
+            <Select.Option value="0">New</Select.Option>
+            <Select.Option value="1">Good</Select.Option>
+            <Select.Option value="2">Decent</Select.Option>
+            <Select.Option value="3">Used</Select.Option>
+          </Select>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
+
+export default CreateUnlinkedEquipModal;
--- cori-app/src/pages/admin/AdminEquipmentManagement.tsx
+++ cori-app/src/pages/admin/AdminEquipmentManagement.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Import Icons
 import { Icons } from "../../constants/icons";
@@ -6,20 +6,32 @@ import { Icons } from "../../constants/icons";
 // Import Components
 import CoriBtn from "../../components/buttons/CoriBtn";
 
+// Import Modals
+import CreateUnlinkedEquipModal from "../../components/modals/CreateUnlinkedEquipModal";
+
 const AdminEquipmentManagement: React.FC = () => {
+  // Modal States
+  const [showCreateUnlinkedEquipModal, setShowCreateUnlinkedEquipModal] = useState(false);
+
   return (
-    <div className="max-w-7xl mx-auto m-4">
-      <div className="flex justify-between items-center mb-4">
-        <div className="flex items-center gap-2">
-          <Icons.Construction fontSize="large" className="text-zinc-900" />
-          <h1 className="text-3xl font-bold text-zinc-900">Equipment</h1>
+    <>
+      <div className="max-w-7xl mx-auto m-4">
+        <div className="flex justify-between items-center mb-4">
+          <div className="flex items-center gap-2">
+            <Icons.Construction fontSize="large" className="text-zinc-900" />
+            <h1 className="text-3xl font-bold text-zinc-900">Equipment</h1>
+          </div>
+          <CoriBtn style="black" onClick={() => setShowCreateUnlinkedEquipModal(true)}>
+            Create
+            <Icons.Add />
+          </CoriBtn>
         </div>
-        <CoriBtn style="black">
-          Create
-          <Icons.Add />
-        </CoriBtn>
       </div>
-    </div>
+      <CreateUnlinkedEquipModal
+        showModal={showCreateUnlinkedEquipModal}
+        setShowModal={setShowCreateUnlinkedEquipModal}
+      />
+    </>
   );
 };
 
