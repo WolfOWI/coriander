4d8ea121538b458493e9194dfb443743404aa3b9
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-04-17 13:05:15 +0200

    Create DeleteEquipmentModal

---
+++ cori-app/src/components/modals/DeleteEquipmentModal.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Modal, Button } from "antd";
+import { Icons } from "../../constants/icons";
+interface DeleteEquipmentModalProps {
+  showModal: boolean;
+  setShowModal: (show: boolean) => void;
+  equipment: {
+    equipmentId: number;
+    equipmentName: string;
+    equipmentCategoryName: string;
+    assignedDate: string;
+    condition: number;
+  };
+  onDelete: () => void;
+}
+
+function DeleteEquipmentModal({
+  showModal,
+  setShowModal,
+  equipment,
+  onDelete,
+}: DeleteEquipmentModalProps) {
+  return (
+    <Modal
+      title={<h2 className="text-red-600 font-bold text-3xl text-center">Delete Item?</h2>}
+      open={showModal}
+      onCancel={() => setShowModal(false)}
+      width={600}
+      styles={{
+        header: {
+          paddingLeft: 40,
+          paddingRight: 40,
+          paddingTop: 40,
+          background: "#F5F5F4", // warmstone-100 equivalent
+          borderBottom: "none",
+        },
+        body: {
+          paddingLeft: 40,
+          paddingRight: 40,
+        },
+        footer: {
+          paddingLeft: 40,
+          paddingRight: 40,
+          paddingBottom: 40,
+          borderTop: "none",
+        },
+      }}
+      footer={[
+        <div className="flex gap-2 w-full" key="footer">
+          <Button className="w-full" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button className="w-full" type="primary" danger onClick={onDelete}>
+            Delete
+          </Button>
+        </div>,
+      ]}
+    >
+      <div className="flex flex-col gap-2 text-center">
+        <p className="text-zinc-500 text-sm">This cannot be undone.</p>
+        <div className="flex items-center gap-3 px-4 py-3 bg-warmstone-300 rounded-xl my-2">
+          <div className="bg-warmstone-50 rounded-full p-2">
+            <Icons.Phone className="text-zinc-900" fontSize="large" />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-zinc-900">{equipment.equipmentName}</p>
+            <div className="flex items-center gap-2">
+              <p className="text-zinc-500 text-sm">{equipment.equipmentCategoryName}</p>
+              <p className="text-zinc-500 text-sm">•</p>
+              <p className="text-zinc-500 text-sm">
+                {equipment.condition === 0
+                  ? "New"
+                  : equipment.condition === 1
+                  ? "Good"
+                  : equipment.condition === 2
+                  ? "Decent"
+                  : "Used"}{" "}
+                Quality
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default DeleteEquipmentModal;
--- cori-app/src/pages/admin/AdminEquipmentManagement.tsx
+++ cori-app/src/pages/admin/AdminEquipmentManagement.tsx
@@ -10,13 +10,26 @@ import CoriBtn from "../../components/buttons/CoriBtn";
 import CreateUnlinkedEquipModal from "../../components/modals/CreateUnlinkedEquipModal";
 import EditEquipDetailsModal from "../../components/modals/EditEquipDetailsModal";
 import AssignSingleEquipToEmpModal from "../../components/modals/AssignSingleEquipToEmpModal";
+import DeleteEquipmentModal from "../../components/modals/DeleteEquipmentModal";
+
+// TODO: Temporary data here
+const equipment = [
+  {
+    equipmentId: 1,
+    equipmentName: "MacBook Pro 13 inch 2021",
+    equipmentCategoryId: 1,
+    equipmentCategoryName: "Laptop",
+    assignedDate: "2021-01-01",
+    condition: 2,
+  },
+];
 
 const AdminEquipmentManagement: React.FC = () => {
   // Modal States
   const [showCreateUnlinkedEquipModal, setShowCreateUnlinkedEquipModal] = useState(false);
   const [showEditEquipDetailsModal, setShowEditEquipDetailsModal] = useState(false);
   const [showAssignSingleEquipToEmpModal, setShowAssignSingleEquipToEmpModal] = useState(false);
-
+  const [showDeleteEquipmentModal, setShowDeleteEquipmentModal] = useState(false);
   return (
     <>
       <div className="max-w-7xl mx-auto m-4">
@@ -31,6 +44,9 @@ const AdminEquipmentManagement: React.FC = () => {
           </CoriBtn>
           <CoriBtn secondary style="black" onClick={() => setShowAssignSingleEquipToEmpModal(true)}>
             Assign2Emp Modal
+          </CoriBtn>
+          <CoriBtn secondary style="black" onClick={() => setShowDeleteEquipmentModal(true)}>
+            Delete Equip Modal
           </CoriBtn>
           <CoriBtn style="black" onClick={() => setShowCreateUnlinkedEquipModal(true)}>
             Create
@@ -49,6 +65,14 @@ const AdminEquipmentManagement: React.FC = () => {
       <AssignSingleEquipToEmpModal
         showModal={showAssignSingleEquipToEmpModal}
         setShowModal={setShowAssignSingleEquipToEmpModal}
+      />
+      <DeleteEquipmentModal
+        showModal={showDeleteEquipmentModal}
+        setShowModal={setShowDeleteEquipmentModal}
+        equipment={equipment[0]}
+        onDelete={() => {
+          console.log("Delete");
+        }}
       />
     </>
   );
