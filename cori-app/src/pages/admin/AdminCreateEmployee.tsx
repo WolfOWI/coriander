a99d0feec43b581064a276ee04815f2248efe8bb
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-04-20 14:31:51 +0200

    Add comments to explain Assign Modal on CreateEmp

--- cori-app/src/pages/admin/AdminCreateEmployee.tsx
+++ cori-app/src/pages/admin/AdminCreateEmployee.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import AssignEmpToEquipsModal from "../../components/modals/AssignEmpToOneOrManyEquipsModal";
+import AssignEmpToOneOrManyEquipsModal from "../../components/modals/AssignEmpToOneOrManyEquipsModal";
 import CoriBtn from "../../components/buttons/CoriBtn";
 
 const AdminCreateEmployee: React.FC = () => {
@@ -9,11 +9,19 @@ const AdminCreateEmployee: React.FC = () => {
     <div className="max-w-7xl mx-auto m-4">
       <h1 className="text-3xl font-bold mb-6 text-zinc-900">Create Employee</h1>
       <CoriBtn style="black" onClick={() => setShowAssignEmpToEquipsModal(true)}>
-        Assign Employee to Equipment
+        Assign 1 or Multiple Equipments
       </CoriBtn>
-      <AssignEmpToEquipsModal
+      {/* Assign Multiple Existing Equipments Modal */}
+      {/* TODO: Will probably create a copy of this modal, since this modal assigns immediately on press of Assign Button. */}
+      <AssignEmpToOneOrManyEquipsModal
         showModal={showAssignEmpToEquipsModal}
         setShowModal={setShowAssignEmpToEquipsModal}
+        employeeId={8}
+        onAssignSuccess={() => {
+          console.log(
+            "Callback function that will be called when the employee is assigned to the equipment"
+          );
+        }}
       />
     </div>
   );
