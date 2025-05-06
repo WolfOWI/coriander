7c2b0bfd5b598305e7a18e50cc5f7b98b01607cf
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-05-06 12:45:48 +0200

    Implement frontend of Apply for leave Modal

--- cori-app/src/pages/TempModalsLeaveOverviewPage.tsx
+++ cori-app/src/pages/TempModalsLeaveOverviewPage.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import CoriBtn from "../components/buttons/CoriBtn";
+import ApplyForLeaveModal from "../components/modals/ApplyForLeaveModal";
 
 function TempModalsLeaveOverviewPage() {
+  const [showApplyForLeaveModal, setShowApplyForLeaveModal] = useState(false);
+
   return (
     <div className="max-w-7xl mx-auto m-4">
       <h1 className="text-3xl font-bold mb-6 text-zinc-900">Modal on Leave Overview</h1>
+      <CoriBtn onClick={() => setShowApplyForLeaveModal(true)}>Apply for Leave Modal</CoriBtn>
+      <ApplyForLeaveModal
+        showModal={showApplyForLeaveModal}
+        setShowModal={setShowApplyForLeaveModal}
+        onSubmitSuccess={() => {
+          ("Functionality not implemented yet");
+        }}
+      />
     </div>
   );
 }
