864ca52209c8635224de05f3bd08cde4c281b900
KaylaPosthumus <kaylaposthu@gmail.com>
2025-05-28 17:08:58 +0200

    Pmplement Correct PendingLeaveRequests on AdminDashboard

--- cori-app/src/components/leave/LeaveCardAdminDash.tsx
+++ cori-app/src/components/leave/LeaveCardAdminDash.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-  interface LeaveRequest {
-    leaveRequestId: number;
-    employeeId: number;
-    employeeName: string;
-    startDate: string;
-    endDate: string;
-    leaveType: string;
-    createdAt: string;
-  }
+interface PendingLeaveRequest {
+  leaveRequestId: number;
+  employeeId: number;
+  fullName: string;
+  startDate: string;
+  endDate: string;
+  leaveTypeName: string;
+  createdAt: string;
+}
 
-  interface LeaveCardAdminDashProps {
-    leave: LeaveRequest;
-  }
+interface LeaveCardAdminDashProps {
+  leave: PendingLeaveRequest;
+}
 
 const LeaveCardAdminDash: React.FC<LeaveCardAdminDashProps> = ({ leave }) => {
   const start = new Date(leave.startDate);
@@ -21,11 +21,11 @@ const LeaveCardAdminDash: React.FC<LeaveCardAdminDashProps> = ({ leave }) => {
 
   return (
     <div className="hover:bg-sakura-500 p-3 rounded-xl flex flex-col border-2 border-sakura-500 group transition-colors duration-200">
-      <h4 className="font-semibold text-md">{leave.employeeName}</h4>
+      <h4 className="font-semibold text-md">{leave.fullName}</h4>
       <p className="text-xs">Submitted on {new Date(leave.createdAt).toLocaleDateString()} </p>
 
-      <div className="hidden-0 max-h-0 overflow-hidden group-hover:hidden-1 group-hover:max-h-44 group-hover:mt-2 transition-all duration-300 ease-in-out">
-        <h4 className="text-zinc-900 font-medium text-sm">Leave Type: {leave.leaveType}</h4>
+      <div className="hidden-0 max-h-0 pointer-events-none overflow-hidden group-hover:hidden-100 group-hover:max-h-96 group-hover:pointer-events-auto group-hover:mt-2 transition-all duration-300 ease-in-out">
+        <h4 className="text-zinc-900 font-medium text-sm">Leave Type: {leave.leaveTypeName}</h4>
         <div className="flex justify-between font-light text-sm">
           <p>
             {new Date(leave.startDate).toLocaleDateString()} - {new Date(leave.endDate).toLocaleDateString()}
