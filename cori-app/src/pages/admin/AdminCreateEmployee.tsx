aaaf068aff9821ad1bf1353e096b81c0981c4e53
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-06-08 14:13:44 +0200

    Small UX Design Tweaks

--- cori-app/src/pages/admin/AdminCreateEmployee.tsx
+++ cori-app/src/pages/admin/AdminCreateEmployee.tsx
@@ -12,13 +12,11 @@ import {
 } from "antd";
 const { Title, Text } = Typography;
 import CoriBtn from "../../components/buttons/CoriBtn";
-import EquipmentListItem from "../../components/equipment/EquipmentListItem";
+import EquipAssignListItem from "../../components/equipment/EquipAssignListItem";
 import AssignEmpToOneOrManyEquipsModal from "../../components/modals/AssignEmpToOneOrManyEquipsModal";
 import { Icons } from "../../constants/icons";
 import { EquipmentCondition, EquipmentCategory } from "../../types/common";
-import UnlinkedUserDropdown, {
-  UnlinkedUser,
-} from "../../components/dropdown/UnlinkedUserDropdown";
+import UnlinkedUserDropdown, { UnlinkedUser } from "../../components/dropdown/UnlinkedUserDropdown";
 import { employeeAPI, userAPI } from "../../services/api.service";
 import dayjs from "dayjs";
 
@@ -31,8 +29,7 @@ const AdminCreateEmployee: React.FC = () => {
   const [loadingUsers, setLoadingUsers] = useState(true);
   const [unlinkedUsers, setUnlinkedUsers] = useState<UnlinkedUser[]>([]);
   const [selectedUser, setSelectedUser] = useState<UnlinkedUser | null>(null);
-  const [showAssignEmpToEquipsModal, setShowAssignEmpToEquipsModal] =
-    useState(false);
+  const [showAssignEmpToEquipsModal, setShowAssignEmpToEquipsModal] = useState(false);
   const [equipmentIds, setEquipmentIds] = useState<number[]>([]); // placeholder
   const [selectedEquipments, setSelectedEquipments] = useState<Equipment[]>([]);
   const [messageApi, contextHolder] = message.useMessage();
@@ -82,9 +79,7 @@ const AdminCreateEmployee: React.FC = () => {
               <Title level={4} className="mb-2">
                 Loading Users
               </Title>
-              <Text type="secondary">
-                Please hold on while we fetch your data…
-              </Text>
+              <Text type="secondary">Please hold on while we fetch your data…</Text>
             </div>
           }
         />
@@ -152,9 +147,7 @@ const AdminCreateEmployee: React.FC = () => {
       messageApi.open({
         key: messageKey,
         type: "error",
-        content: `${
-          err.response?.data?.message || "Failed to create employee"
-        }`,
+        content: `${err.response?.data?.message || "Failed to create employee"}`,
       });
     }
   };
@@ -189,10 +182,7 @@ const AdminCreateEmployee: React.FC = () => {
       {/* Empty state */}
       {!selectedUser && (
         <div className="pt-32 text-center text-gray-500">
-          <Icons.PersonAdd
-            className="text-corigreen-500"
-            style={{ fontSize: 64 }}
-          />
+          <Icons.PersonAdd className="text-corigreen-500" style={{ fontSize: 64 }} />
           <h2 className="text-xl font-semibold mt-2">No User Selected</h2>
           <p className="mt-2">Please select a user above to begin.</p>
         </div>
@@ -200,12 +190,7 @@ const AdminCreateEmployee: React.FC = () => {
 
       {/* Combined Form */}
       {selectedUser && (
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={handleCreateEmployee}
-          className="space-y-8"
-        >
+        <Form form={form} layout="vertical" onFinish={handleCreateEmployee} className="space-y-8">
           <Form.Item name="userId" initialValue={selectedUser.userId} hidden>
             <Input />
           </Form.Item>
@@ -213,14 +198,8 @@ const AdminCreateEmployee: React.FC = () => {
           {/* Personal Details */}
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div>
-              <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">
-                Personal
-              </div>
-              <Form.Item
-                name="gender"
-                label="Gender"
-                rules={[{ required: true }]}
-              >
+              <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">Personal</div>
+              <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
                 <Select
                   options={[
                     { label: "Male", value: 0 },
@@ -230,18 +209,10 @@ const AdminCreateEmployee: React.FC = () => {
                   className="drop-shadow-md"
                 />
               </Form.Item>
-              <Form.Item
-                name="phone"
-                label="Phone Number"
-                rules={[{ required: true }]}
-              >
+              <Form.Item name="phone" label="Phone Number" rules={[{ required: true }]}>
                 <Input className="drop-shadow-md" />
               </Form.Item>
-              <Form.Item
-                name="dob"
-                label="Date of Birth"
-                rules={[{ required: true }]}
-              >
+              <Form.Item name="dob" label="Date of Birth" rules={[{ required: true }]}>
                 <DatePicker className="w-full h-12 drop-shadow-md" />
               </Form.Item>
             </div>
@@ -251,18 +222,10 @@ const AdminCreateEmployee: React.FC = () => {
                 <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">
                   Employment
                 </div>
-                <Form.Item
-                  name="jobTitle"
-                  label="Job Title"
-                  rules={[{ required: true }]}
-                >
+                <Form.Item name="jobTitle" label="Job Title" rules={[{ required: true }]}>
                   <Input className="drop-shadow-md" />
                 </Form.Item>
-                <Form.Item
-                  name="department"
-                  label="Department"
-                  rules={[{ required: true }]}
-                >
+                <Form.Item name="department" label="Department" rules={[{ required: true }]}>
                   <Input className="drop-shadow-md" />
                 </Form.Item>
                 <Form.Item
@@ -280,35 +243,18 @@ const AdminCreateEmployee: React.FC = () => {
                     ]}
                   />
                 </Form.Item>
-                <Form.Item
-                  name="startDate"
-                  label="Date of Employment"
-                  rules={[{ required: true }]}
-                >
+                <Form.Item name="startDate" label="Date of Employment" rules={[{ required: true }]}>
                   <DatePicker className="w-full h-12 drop-shadow-md" />
                 </Form.Item>
               </div>
 
               {/* Payroll */}
               <div className="pt-2">
-                <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">
-                  Payroll
-                </div>
-                <Form.Item
-                  name="salary"
-                  label="Salary Amount"
-                  rules={[{ required: true }]}
-                >
-                  <InputNumber
-                    prefix="R"
-                    className="w-full h-12 rounded-lg drop-shadow-md"
-                  />
+                <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">Payroll</div>
+                <Form.Item name="salary" label="Salary Amount" rules={[{ required: true }]}>
+                  <InputNumber prefix="R" className="w-full h-12 rounded-lg drop-shadow-md" />
                 </Form.Item>
-                <Form.Item
-                  name="payCycle"
-                  label="Pay Cycle"
-                  rules={[{ required: true }]}
-                >
+                <Form.Item name="payCycle" label="Pay Cycle" rules={[{ required: true }]}>
                   <Select
                     className="drop-shadow-md"
                     options={[
@@ -322,38 +268,31 @@ const AdminCreateEmployee: React.FC = () => {
             </div>
             {/* Equipment Section */}
             <div className="flex flex-col items-center ">
-              <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">
-                Equipment
-              </div>
+              <div className="text-zinc-500 font-semibold text-center mb-2 text-lg">Equipment</div>
               {/* If selected equipment ids (equipmentIds) is empty it must show, please add equpment */}
               {/* You have to select the equipment items from the equipments modal */}
               <div className="space-y-4 w-full mt-2">
                 {equipmentIds.length === 0 ? (
                   <div className="flex gap-2 items-center justify-center">
-                    <Icons.Warning
-                      className="text-yellow-500"
-                      style={{ fontSize: 28 }}
-                    />
+                    <Icons.Warning className="text-yellow-500" style={{ fontSize: 28 }} />
                     <div>
                       <Text strong className="text-yellow-800 block">
                         No equipment selected
                       </Text>
                       <Text type="secondary">
-                        The employee will be registered without any equipment.
-                        Click "Add Equipment" to assign.
+                        The employee will be registered without any equipment. Click "Add Equipment"
+                        to assign.
                       </Text>
                     </div>
                   </div>
                 ) : (
-                  <div className="bg-white rounded-lg drop-shadow-md p-4 border border-zinc-500">
+                  <div className="bg-white rounded-2xl drop-shadow-md p-4 border border-zinc-500 flex flex-col gap-4">
                     {selectedEquipments.map((equip) => (
-                      <EquipmentListItem
+                      <EquipAssignListItem
                         key={equip.equipmentId}
                         item={equip}
                         adminView
-                        onDelete={() =>
-                          handleRemoveEquipment(equip.equipmentId)
-                        }
+                        onDelete={() => handleRemoveEquipment(equip.equipmentId)}
                         onEdit={() => {}}
                         onUnlink={() => {}}
                       />
